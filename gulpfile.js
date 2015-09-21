83f17a453b2a41ecf615874131ffc74746ec5c32
Marcin Chwedczuk <chwedczukm@yahoo.com>
2015-09-21 20:00:35 +0200

    Split format.js to format.js and scan.js

---
+++ gulpfile.js
@@ -0,0 +1,89 @@
+(function() {
+    'use strict';
+
+    // based on: http://blog.nodejitsu.com/npmawesome-9-gulp-plugins/
+
+    var gulp = require('gulp');
+    var clean = require('gulp-clean');
+    var concat = require('gulp-concat');
+    var uglify = require('gulp-uglify');
+    var gutil = require('gulp-util');
+    var rename = require('gulp-rename');
+    var filesize = require('gulp-filesize');
+    var watch = require('gulp-watch');
+    var batch = require('gulp-batch');
+    var mocha = require('gulp-mocha');
+    var jshint = require('gulp-jshint');
+    var betterConsole = require('better-console');
+    var source = require('vinyl-source-stream');
+    var browserify = require('browserify');
+
+    var SOURCE_DIR = 'lib';
+    var DEST_DIR = 'dist';
+    var TEST_DIR = 'test';
+
+    var USER_LIBRARY_NAME = 'scan';
+    var SCAN_LIBRARY_NAME = USER_LIBRARY_NAME + '.js';
+    var SCAN_LIBRARY_NAME_MIN = SCAN_LIBRARY_NAME.replace('.js', '.min.js');
+    var BROWSERIFIED_LIBRARY_NAME = SCAN_LIBRARY_NAME.replace('.js', '.browserified.js');
+
+    var SOURCE_JS_FILES = SOURCE_DIR + '/*.js';
+    var TEST_JS_FILES = TEST_DIR + '/*.js';
+    var DEST_JS_FILES = DEST_DIR + '/*.js';
+
+    var BROWSERIFIED_JS_FILES = DEST_DIR + '/' + BROWSERIFIED_LIBRARY_NAME;
+
+    var JSHINT_REPORTER = 'jshint-stylish';
+
+    gulp.task('jshint', function() {
+        return gulp.src([SOURCE_JS_FILES, TEST_JS_FILES])
+            .pipe(jshint())
+            .pipe(jshint.reporter(JSHINT_REPORTER))
+            .pipe(jshint.reporter('fail'))
+    });
+
+    gulp.task('browserify', function() {
+        var b = browserify(['index.js'], { 
+            debug: true,
+            standalone: USER_LIBRARY_NAME
+        });
+
+        return b.bundle()
+            .pipe(source(BROWSERIFIED_LIBRARY_NAME))
+            .pipe(gulp.dest(DEST_DIR));
+    });
+
+    gulp.task('build', ['jshint', 'browserify'], function() {
+        return gulp.src(BROWSERIFIED_JS_FILES)
+            .pipe(gulp.dest(DEST_DIR))
+            .pipe(uglify())
+            .pipe(rename(SCAN_LIBRARY_NAME_MIN))
+            .pipe(gulp.dest(DEST_DIR))
+            .pipe(filesize())
+            .on('error', gutil.log);
+    });
+
+    gulp.task('test', ['build'], function() {
+        return gulp.src(TEST_JS_FILES, { read: false })
+            .pipe(mocha({
+                style: 'bdd',
+                reporter: 'nyan',
+            }));
+    });
+
+    gulp.task('clean', function() {
+        return gulp.src(DEST_JS_FILES, { read: false })
+            .pipe(clean())
+            .on('error', gutil.log);
+    });
+
+    gulp.task('watch', function() {
+        watch([SOURCE_JS_FILES, TEST_JS_FILES], batch(function(events, done) {
+            betterConsole.clear();
+            gulp.start('test', done);
+        }));   
+    });
+    
+    gulp.task('default', ['build'], function() {});
+
+}());
